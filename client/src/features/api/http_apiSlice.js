fc7d8daabb9cc1ef639bc3bde4b201f7adb718a4
Adam D. <adam@adamduda.com>
2024-05-17 12:44:26 -0500

    update

--- client/src/features/api/http_apiSlice.js
+++ client/src/features/api/http_apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "http://3.139.77.236:3000",
   prepareHeaders: (headers, { getState }) => {
     const token = localStorage.getItem('token');
     if (token) {
